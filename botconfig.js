ff2cdd98bbbed95bd5365c122dc31ca591dcc490
PawnStormerD4 <102794507+PawnCodez@users.noreply.github.com>
2023-08-13 19:55:56 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,15 +4,15 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || "=", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/pawnsmp", // Support Server Link
-  Token: process.env.Token || "OTQxNTMzNTE0MDE5NTIwNTYz.G10K5R.vnAg37lbtJuOJJCa9rEmxnPknmoGc5tvRnPksE", // Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "941533514019520563", // Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "nQFzWcytm5hYrWE07AQfTwpcI-YSsbgz", // Discord Client Secret
+  Token: process.env.Token || "###############################################", // Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "###############################################", // Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "###############################################", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
   ServerDeafen: true, // If you want bot to stay deafened
   DefaultVolume: 100, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
   CallbackURL: "/api/callback", // Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
   "24/7": false, // Make the bot stays in VC 24/7 (when you reboot the bot will **not** automatically rejoin.)
-  CookieSecret: "PS Music is OP", // A cookie for you, cookie for me. make sure you change this value!
+  CookieSecret: "###############################################", // A cookie for you, cookie for me. make sure you change this value!
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", // URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", // Color of most embeds | Custom Hex value are supported. I.e: "#36393F"
@@ -29,17 +29,17 @@ module.exports = {
   // You need a lavalink server for this bot to work!!!!
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
   Lavalink: {
-    id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "lava1.horizxon.studio", //- The host name or IP of the lavalink server.
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "horizxon.studio", //- The password of the lavalink server.
-    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
+    id: "###############################################", //- Used for indentifier. You can set this to whatever you want.
+    host: "###############################################", //- The host name or IP of the lavalink server.
+    port: ###############################################, // The port that lavalink is listening to. This must be a number!
+    pass: "###############################################", //- The password of the lavalink server.
+    secure: ###############################################, // Set this to true if the lavalink uses SSL. if not set it to false.
     retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
     retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
   },
   // Spotify Integration, allows you to enter a spotify link.
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "bbf8d69adb964b128df793d2d9d233fe", // Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "6ad20ba86bfc45cf897d68ebe118b236", // Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "###############################################", // Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "###############################################", // Spotify Client Secret
   },
 };
